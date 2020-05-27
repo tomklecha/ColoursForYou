f9c86f3b0870fc44428ca21106e2ad9fc664e9c5
tomklecha <tom.klecha.dev@gmail.com>
2020-05-27 21:56:16 +0100

    *added on view created tests to presenter tests

--- app/src/main/java/com/tkdev/coloursforyou/presenter/ColoursPresenter.kt
+++ app/src/main/java/com/tkdev/coloursforyou/presenter/ColoursPresenter.kt
@@ -1,3 +1,31 @@
 package com.tkdev.coloursforyou.presenter
 
-class ColoursPresenter {}
+import com.tkdev.coloursforyou.core.ColoursContract
+import com.tkdev.coloursforyou.core.ColoursInteractor
+
+class ColoursPresenter(
+    private val interactor: ColoursInteractor
+) : ColoursContract.Presenter {
+
+    private lateinit var view: ColoursContract.View
+
+    override fun bind(view: ColoursContract.View) {
+      this.view = view
+    }
+
+    override fun unbind() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewCreated() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onButtonClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewSwiped() {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/test/java/com/tkdev/coloursforyou/presenter/ColoursPresenterTest.kt
+++ app/src/test/java/com/tkdev/coloursforyou/presenter/ColoursPresenterTest.kt
@@ -1,5 +1,55 @@
 package com.tkdev.coloursforyou.presenter
 
-import org.junit.Assert.*
+import com.tkdev.coloursforyou.core.ColoursContract
+import com.tkdev.coloursforyou.data.model.Colour
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import org.junit.Before
+import org.junit.Test
 
-class ColoursPresenterTest
+class ColoursPresenterTest{
+
+    @MockK
+    private lateinit var view: ColoursContract.View
+
+    @MockK
+    private lateinit var interactor: ColoursContract.Interactor
+
+    @InjectMockKs
+    private lateinit var presenter: ColoursPresenter
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxed = true)
+        presenter.bind(view)
+    }
+
+
+    @Test
+    fun `GIVEN no saved data, WHEN on view created, THEN show message no saved data`(){
+        //GIVEN
+        val message = "not this time my friend"
+
+        //WHEN
+        presenter.onViewCreated()
+
+        //THEN
+        view.showError(message)
+    }
+
+    @Test
+    fun `GIVEN saved data, WHEN on view created, THEN update view with data`(){
+        //GIVEN
+        val list : List<Colour> = listOf(Colour("harry", -1), Colour("Potter", -1))
+
+        every { interactor.getSavedColours() } returns list
+
+        //WHEN
+        presenter.onViewCreated()
+
+        //THEN
+        view.updateCurrentColours(list)
+    }
+}

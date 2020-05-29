76b15d9a81dd37f8c9ce1ea6b4606bd2d617fe3f
tomklecha <tom.klecha.dev@gmail.com>
2020-05-29 21:29:06 +0100

    *partial implementation of interactor

--- app/src/main/java/com/tkdev/coloursforyou/core/ColoursInteractor.kt
+++ app/src/main/java/com/tkdev/coloursforyou/core/ColoursInteractor.kt
@@ -7,11 +7,32 @@ class ColoursInteractor(
 ) : ColoursContract.Interactor {
 
     override fun getSavedColours(): List<Colour> {
-        TODO("Not yet implemented")
+       return when(val result = repository.getSavedColours()){
+            emptyList<Colour>() -> emptyList()
+            else -> result
+        }
     }
 
     override fun generateColours(): List<Colour> {
-        TODO("Not yet implemented")
+      return when(val result = repository.getWords()){
+            emptyList<String>() -> emptyList()
+            else -> createColourList(result)
+        }
+    }
+
+    private fun createColourList(list: List<String>): List<Colour> {
+        val colourList = emptyList<Colour>()
+
+        list.forEach { i ->
+
+        }
+
+        return colourList
+    }
+
+    private fun randomizeColor() : String{
+
+        return ""
     }
 
 }

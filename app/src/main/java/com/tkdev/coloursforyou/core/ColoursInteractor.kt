65d2f5037326a6a11ea57abc7c0af83a7f5e79eb
tomklecha <tom.klecha.dev@gmail.com>
2020-05-30 09:51:05 +0100

    created color generator - not passing the test, wrongly coked in test - TODO !

--- app/src/main/java/com/tkdev/coloursforyou/core/ColoursInteractor.kt
+++ app/src/main/java/com/tkdev/coloursforyou/core/ColoursInteractor.kt
@@ -1,6 +1,9 @@
 package com.tkdev.coloursforyou.core
 
 import com.tkdev.coloursforyou.data.model.Colour
+import kotlin.random.Random
+import kotlin.text.StringBuilder
+
 
 class ColoursInteractor(
     private val repository: ColoursContract.Repository
@@ -21,18 +24,23 @@ class ColoursInteractor(
     }
 
     private fun createColourList(list: List<String>): List<Colour> {
-        val colourList = emptyList<Colour>()
+        val colourList = ArrayList<Colour>()
 
-        list.forEach { i ->
-
+        list.forEach {
+            colourList.add(Colour(it,randomizeColor()))
         }
 
-        return colourList
+        return colourList.toList()
     }
 
     private fun randomizeColor() : String{
-
-        return ""
+        val hexVariables = listOf("0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F")
+        val builder = StringBuilder()
+        builder.append("#")
+        for (i in 0..5){
+            builder.append( hexVariables[Random.nextInt(0,15)])
+        }
+        return builder.toString()
     }
 
 }

57f6939ac5b25fccb1c68f49670968995505fc12
tomklecha <tom.klecha.dev@gmail.com>
2020-05-30 10:22:10 +0100

    *added concept implementation of repository

--- app/src/main/java/com/tkdev/coloursforyou/data/ColoursRepository.kt
+++ app/src/main/java/com/tkdev/coloursforyou/data/ColoursRepository.kt
@@ -1,6 +1,8 @@
 package com.tkdev.coloursforyou.data
 
 import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.tkdev.coloursforyou.core.ColoursContract
 import com.tkdev.coloursforyou.data.model.Colour
 import com.tkdev.coloursforyou.data.model.ColoursApi
@@ -11,14 +13,35 @@ class ColoursRepository(
 ) : ColoursContract.Repository {
 
     override fun getSavedColours(): List<Colour> {
-        TODO("Not yet implemented")
+        var colourList: List<Colour> = emptyList()
+        with(sharedPreferences) {
+            val gson = Gson()
+            val json = sharedPreferences.getString("list", "")
+            val type = object : TypeToken<List<Colour>>() {}.type
+            if (json != null){
+            colourList = gson.fromJson(json, type)
+            }
+        }
+
+        return when (colourList.isEmpty()) {
+            true -> emptyList()
+            false -> colourList.toList()
+        }
     }
 
     override fun getWords(): List<String> {
-        TODO("Not yet implemented")
+        return try {
+            api.fetchData().toList()
+        } catch (e: ExceptionInInitializerError) {
+            emptyList()
+        }
     }
 
     override fun saveGeneratedColours(colours: List<Colour>) {
-        TODO("Not yet implemented")
+        with(sharedPreferences.edit()){
+            val gson = Gson()
+            val json = gson.toJson(colours)
+            putString("list", json)
+        }.apply()
     }
 }
